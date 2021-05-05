2a80e59b924fa01df32015d955551d83d5182ddb
doguserdem <doguserdem9@gmail.com>
2021-05-05 18:33:43 +0300

    05.05.2021

--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -1,7 +1,9 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +18,15 @@ namespace Business.Constans
         public static string ProductCountOfCategoryError = "Bir kategoriden en fazla 10 ürün olabilir";
         public static string ProductNameAlreadyExists="Bu isimde bir ürün zaten var";
         public static string CategoryLimitExceted= "Kategori limiti aşıldı";
+        public static string AuthorizationDenied = "Yetkilendirme reddedildi!";
+
+        public static string UserRegistered = "Kayıt olundu!";
+        public static string UserNotFound = "Kullanıcı bulunamadı!";
+        public static string SuccessfulLogin = "Başarılı Giriş!";
+        public static string PasswordError = "Şifre Hatalı!";
+        public static string UserAlreadyExists = "Kullanıcı zaten var!";
+        public static string AccessTokenCreated = "Erişim sağlandı!";
     }
+        
 }
+
